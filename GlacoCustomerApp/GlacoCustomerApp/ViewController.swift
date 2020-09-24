52dbeedbfdf15959299bb8b5766c267807986d90
Kato <klefebvre90@gmail.com>
2020-09-23 22:52:29 -0400

    Initial creation

---
+++ GlacoCustomerApp/GlacoCustomerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlacoCustomerApp
+//
+//  Created by Kato Lefebvre on 2020-09-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
