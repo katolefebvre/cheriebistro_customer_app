0cc4f77d365fbfb1b064bacf363945708aa803ed
Karl <galinskk@sheridancollege.ca>
2020-09-29 19:08:48 +0200

    Remove from cart functionality and cart formatting

--- GlacoCustomerApp/GlacoCustomerApp/Models/ShoppingCartTableViewCell.swift
+++ GlacoCustomerApp/GlacoCustomerApp/Models/ShoppingCartTableViewCell.swift
@@ -9,15 +9,70 @@ import UIKit
 
 class ShoppingCartTableViewCell: UITableViewCell {
 
+//    @IBOutlet var itemName : UILabel!
+//    @IBOutlet var itemDesc : UILabel!
+//    @IBOutlet var itemPrice : UILabel!
+//    @IBOutlet var removeButton : UIButton!
+    
+    let itemName = UILabel()
+    let itemDesc = UILabel()
+    let itemPrice = UILabel()
+    let removeButton = UIButton()
+    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    var parentDelegate: ParentControllerDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        itemName.textAlignment = .left
+        itemName.font = UIFont.boldSystemFont(ofSize: 25)
+        itemName.backgroundColor = .clear
+        itemName.textColor = .black
+        
+        itemDesc.textAlignment = .left
+        itemDesc.font = UIFont.boldSystemFont(ofSize: 16)
+        itemDesc.backgroundColor = .clear
+        itemDesc.textColor = .black
+        
+        itemPrice.textAlignment = .left
+        itemPrice.font = UIFont.boldSystemFont(ofSize: 16)
+        itemPrice.backgroundColor = .clear
+        itemPrice.textColor = .black
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        removeButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        removeButton.setTitle("Remove", for: .normal)
+        
+        contentView.addSubview(itemName)
+        contentView.addSubview(itemDesc)
+        contentView.addSubview(itemPrice)
+        contentView.addSubview(removeButton)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        mainDelegate.ShoppingCart.remove(at: sender.tag)
+        self.parentDelegate?.requestReloadTable()
+    }
 
+    override func layoutSubviews() {
+        itemName.frame = CGRect(x: 15, y:10, width: 200, height: 30)
+        itemDesc.frame = CGRect(x: 210, y: 17, width: 750, height: 20)
+        itemPrice.frame = CGRect(x: 1000, y: 17, width: 90, height: 20)
+        removeButton.frame = CGRect(x: 1100, y: 10, width: 80, height: 30)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
 }
--- GlacoCustomerApp/GlacoCustomerApp/ViewControllers/ShoppingCartViewController.swift
+++ GlacoCustomerApp/GlacoCustomerApp/ViewControllers/ShoppingCartViewController.swift
@@ -2,7 +2,7 @@
 //  ShoppingCartViewController.swift
 //  GlacoCustomerApp
 //
-//  Created by Xcode User on 2020-09-29.
+//  Created by Karl Galinski on 2020-09-29.
 //
 
 import UIKit
@@ -10,28 +10,49 @@ import UIKit
 class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet var tableView : UITableView!
+    @IBOutlet var noItemsMsg : UILabel!
     
     let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mainDelegate.ShoppingCart.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        return 50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableCell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
-        tableCell.textLabel?.text = mainDelegate.ShoppingCart[indexPath.row].name + "   " + mainDelegate.ShoppingCart[indexPath.row].description
+        let tableCell = tableView.dequeueReusableCell(withIdentifier: "cell") as? ShoppingCartTableViewCell ?? ShoppingCartTableViewCell(style: .default, reuseIdentifier: "cell")
+        tableCell.itemName.text = mainDelegate.ShoppingCart[indexPath.row].name
+        tableCell.itemDesc.text = mainDelegate.ShoppingCart[indexPath.row].description
+        tableCell.removeButton.tag = indexPath.row
+        tableCell.parentDelegate = self
+        
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.currencySymbol = "$"
+        
+        tableCell.itemPrice.text = currencyFormatter.string(from: NSNumber(value: mainDelegate.ShoppingCart[indexPath.row].price))!
         tableCell.backgroundColor = .clear
         return tableCell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateNoItemMsg()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateNoItemMsg(){
+        if mainDelegate.ShoppingCart.isEmpty{
+            noItemsMsg.isHidden = false
+        }
+        else{
+            noItemsMsg.isHidden = true
+        }
     }
 
     /*
@@ -44,4 +65,15 @@ class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableVi
     }
     */
 
+}
+
+protocol ParentControllerDelegate{
+func requestReloadTable()
+}
+
+extension ShoppingCartViewController: ParentControllerDelegate{
+func requestReloadTable() {
+    self.tableView.reloadData()
+    updateNoItemMsg()
+}
 }
