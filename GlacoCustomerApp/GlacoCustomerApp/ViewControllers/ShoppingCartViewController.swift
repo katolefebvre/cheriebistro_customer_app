2f1e047dca5101f9279dedddda7454b8b86c7934
Karl <galinskk@sheridancollege.ca>
2020-09-29 22:34:50 +0200

    total price counter

--- GlacoCustomerApp/GlacoCustomerApp/ViewControllers/ShoppingCartViewController.swift
+++ GlacoCustomerApp/GlacoCustomerApp/ViewControllers/ShoppingCartViewController.swift
@@ -11,9 +11,11 @@ class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableVi
     
     @IBOutlet var tableView : UITableView!
     @IBOutlet var noItemsMsg : UILabel!
+    @IBOutlet var totalPrice : UILabel!
     
     let mainDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    let currencyFormatter = NumberFormatter()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mainDelegate.ShoppingCart.count
@@ -30,10 +32,7 @@ class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableVi
         tableCell.removeButton.tag = indexPath.row
         tableCell.parentDelegate = self
         
-        let currencyFormatter = NumberFormatter()
-        currencyFormatter.usesGroupingSeparator = true
-        currencyFormatter.numberStyle = .currency
-        currencyFormatter.currencySymbol = "$"
+        
         
         tableCell.itemPrice.text = currencyFormatter.string(from: NSNumber(value: mainDelegate.ShoppingCart[indexPath.row].price))!
         tableCell.backgroundColor = .clear
@@ -42,7 +41,13 @@ class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.currencySymbol = "$"
+        
         updateNoItemMsg()
+        updateTotal()
         // Do any additional setup after loading the view.
     }
     
@@ -53,6 +58,14 @@ class ShoppingCartViewController: UIViewController,UITableViewDelegate,UITableVi
         else{
             noItemsMsg.isHidden = true
         }
+    }
+    
+    func updateTotal(){
+        var total : Float = 0.0
+        for item in mainDelegate.ShoppingCart{
+            total += item.price
+        }
+        totalPrice.text = currencyFormatter.string(from: NSNumber(value: total))!
     }
 
     /*
@@ -72,8 +85,9 @@ func requestReloadTable()
 }
 
 extension ShoppingCartViewController: ParentControllerDelegate{
-func requestReloadTable() {
-    self.tableView.reloadData()
-    updateNoItemMsg()
-}
+    func requestReloadTable() {
+        self.tableView.reloadData()
+        updateNoItemMsg()
+        updateTotal()
+    }
 }
