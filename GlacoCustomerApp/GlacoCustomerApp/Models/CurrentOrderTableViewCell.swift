510b2f8d6edc34cbec1c3c275fcfe284819108b9
Karl <galinskk@sheridancollege.ca>
2020-10-08 20:54:09 +0200

    Merged shopping cart (now current order) into master.

---
+++ GlacoCustomerApp/GlacoCustomerApp/Models/CurrentOrderTableViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  CurrentOrderTableViewCell.swift
+//  GlacoCustomerApp
+//
+//  Created by Xcode User on 2020-10-08.
+//
+
+import UIKit
+
+class CurrentOrderTableViewCell: UITableViewCell {
+
+    let itemName = UILabel()
+    let itemDesc = UILabel()
+    let itemPrice = UILabel()
+    let removeButton = UIButton()
+    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    var parentDelegate: ParentControllerDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        itemName.textAlignment = .left
+        itemName.font = UIFont.boldSystemFont(ofSize: 25)
+        itemName.backgroundColor = .clear
+        itemName.textColor = .black
+        
+        itemDesc.textAlignment = .left
+        itemDesc.font = UIFont.boldSystemFont(ofSize: 16)
+        itemDesc.backgroundColor = .clear
+        itemDesc.textColor = .black
+        
+        itemPrice.textAlignment = .left
+        itemPrice.font = UIFont.boldSystemFont(ofSize: 16)
+        itemPrice.backgroundColor = .clear
+        itemPrice.textColor = .black
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        removeButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        removeButton.setTitle("Remove", for: .normal)
+        removeButton.setTitleColor(.black, for: .normal)
+        
+        contentView.addSubview(itemName)
+        contentView.addSubview(itemDesc)
+        contentView.addSubview(itemPrice)
+        contentView.addSubview(removeButton)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        mainDelegate.ShoppingCart.remove(at: sender.tag)
+        self.parentDelegate?.requestReloadTable()
+    }
+
+    override func layoutSubviews() {
+        itemName.frame = CGRect(x: 15, y:10, width: 280, height: 30)
+        itemDesc.frame = CGRect(x: 305, y: 17, width: 830, height: 20)
+        itemPrice.frame = CGRect(x: 1150, y: 17, width: 90, height: 20)
+        removeButton.frame = CGRect(x: 1250, y: 10, width: 75, height: 30)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
---
+++ GlacoCustomerApp/GlacoCustomerApp/ViewControllers/CurrentOrderViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CurrentOrderViewController.swift
+//  GlacoCustomerApp
+//
+//  Created by Xcode User on 2020-10-08.
+//
+
+import UIKit
+
+class CurrentOrderViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet var tableView : UITableView!
+        @IBOutlet var noItemsMsg : UILabel!
+        @IBOutlet var totalPrice : UILabel!
+        
+        let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let currencyFormatter = NumberFormatter()
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return mainDelegate.ShoppingCart.count
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 50
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let tableCell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CurrentOrderTableViewCell ?? CurrentOrderTableViewCell(style: .default, reuseIdentifier: "cell")
+            tableCell.itemName.text = mainDelegate.ShoppingCart[indexPath.row].name
+            tableCell.itemDesc.text = mainDelegate.ShoppingCart[indexPath.row].description
+            tableCell.removeButton.tag = indexPath.row
+            tableCell.parentDelegate = self
+            
+            
+            
+            tableCell.itemPrice.text = currencyFormatter.string(from: NSNumber(value: mainDelegate.ShoppingCart[indexPath.row].price))!
+            tableCell.backgroundColor = .clear
+            return tableCell
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            currencyFormatter.usesGroupingSeparator = true
+            currencyFormatter.numberStyle = .currency
+            currencyFormatter.currencySymbol = "$"
+            
+            updateNoItemMsg()
+            updateTotal()
+            // Do any additional setup after loading the view.
+        }
+        
+        func updateNoItemMsg(){
+            if mainDelegate.ShoppingCart.isEmpty{
+                noItemsMsg.isHidden = false
+            }
+            else{
+                noItemsMsg.isHidden = true
+            }
+        }
+        
+        func updateTotal(){
+            var total : Float = 0.0
+            for item in mainDelegate.ShoppingCart{
+                total += item.price
+            }
+            totalPrice.text = currencyFormatter.string(from: NSNumber(value: total))!
+        }
+
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+
+    }
+
+    protocol ParentControllerDelegate{
+    func requestReloadTable()
+    }
+
+    extension CurrentOrderViewController: ParentControllerDelegate{
+        func requestReloadTable() {
+            self.tableView.reloadData()
+            updateNoItemMsg()
+            updateTotal()
+        }
+}
