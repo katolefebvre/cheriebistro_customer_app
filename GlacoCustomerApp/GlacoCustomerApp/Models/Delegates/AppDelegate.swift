0ea9c9f3e99afb5ff38c4e68bc80999200359d2a
Karl <galinskk@sheridancollege.ca>
2020-10-14 18:59:27 +0200

    Removed unused shopping cart from main delegate and added code to disable the "submit Order " button if the order is empty.

--- GlacoCustomerApp/GlacoCustomerApp/Models/Delegates/AppDelegate.swift
+++ GlacoCustomerApp/GlacoCustomerApp/Models/Delegates/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var loggedTable: Table?
-    var ShoppingCart: [MenuItem] = []
     var tableOrder: TableOrder!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
